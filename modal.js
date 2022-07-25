c53728c2c74fda02802c389a4cc5b6dabf380f6c
Sahil Rajbhandari <105587053+rsahilt@users.noreply.github.com>
2022-07-25 14:55:28 +0545

    Add files via upload
    homepage added

---
+++ modal.js
@@ -0,0 +1,17 @@
+// Modal Setup
+var modal = document.getElementById('modal');
+
+var modalClose = document.getElementById('modal-close');
+modalClose.addEventListener('click', function() { 
+  modal.style.display = "none";
+});
+
+// global handler
+document.addEventListener('click', function (e) { 
+  if (e.target.className.indexOf('modal-target') !== -1) {
+      var img = e.target;
+      var modalImg = document.getElementById("modal-content");
+      modal.style.display = "block";
+      modalImg.src = img.src;
+   }
+});
